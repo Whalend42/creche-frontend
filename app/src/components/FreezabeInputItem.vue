9b36e61ee6829d335ea159e95db65ea93b1379ce
whalend <whalend42@gmail.coml>
2022-11-12 18:01:26 +0100

    improve freezable input with vuetify text-input

--- app/src/components/FreezabeInputItem.vue
+++ app/src/components/FreezabeInputItem.vue
@@ -1,64 +1,48 @@
 <script setup lang="ts">
-    import { ref, onUpdated } from 'vue'
+    import { ref } from 'vue'
 
     const props = defineProps<{
         text: string
     }>()
 
-    let state = ref('saved')
+    let state = ref('editable')
     const text = ref(props.text)
-    const showEditable = ref(false)
+    const defaultText = text.value
+    const inputElement = ref(null)
 
     const saved = () => {
         state.value = "saved"
         console.log("saved")
+        if (text.value ==='') {
+            text.value = defaultText
+        }
     }
     const editable = () => {
         state.value = 'editable'
+        inputElement.value?.focus()
         console.log("editable")
-        //$refs.
     }
-    onUpdated(() => {
-        // text content should be the same as current `count.value`
-        //document.getElementById('someId').focus()
-        //console.log(document.getElementById('someId').textContent)
-    })
 </script>
 
 <template>
-    <label
-        class="mdc-text-field mdc-text-field--outlined mdc-text-field--no-label"
-        v-if="state === 'editable'"
-        @keyup.enter="saved"
-        @blur="saved"
-        @focusout="saved" >
-        <span class="mdc-notched-outline">
-            <span class="mdc-notched-outline__leading"></span>
-            <span class="mdc-notched-outline__trailing"></span>
-        </span>
-        <input id="someId" class="mdc-text-field__input" type="text" aria-label="Label" v-model="text">
-    </label>
-
-    <div
-        class="adjusted-height"
-        v-if="state === 'saved'"
-        @click="editable"
-        @mouseover="showEditable = true"
-        @mouseout="showEditable = false"
-        :class="{ clickable: showEditable }">
-        {{text}}
-    </div>
+    <v-card
+        @click="editable">
+        <v-text-field
+            ref="inputElement"
+            density="compact"
+            variant="solo"
+            single-line
+            label="nom"
+            v-model="text"
+            hide-details
+            :disabled="state === 'saved'"
+            @keyup.enter="saved"
+            @blur="saved"
+        ></v-text-field>
+    </v-card>
 </template>
 
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .clickable {
-        cursor: pointer;
-    }
-    .adjusted-height {
-        height: 56px;
-        line-height: 56px;
-        vertical-align: middle;
-    }
 </style>
